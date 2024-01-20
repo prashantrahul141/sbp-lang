77847d146c11ed290829a26b8bfd163ad8686c0f
Prashant <prashantrahul141@protonmail.com>
2024-01-21 01:14:46 +0530

    using lexer in compile

--- src/app/compile.rs
+++ src/app/compile.rs
@@ -6,9 +6,10 @@ impl App {
     /// Top level compile function, this function
     /// takes source as input string and calls all
     ///  the steps required to compile and run it.
-    pub fn compile(&self, source: &String) {
+    pub fn compile(&self, source: String) {
         // lexical analysis.
         spdlog::debug!("compiling : \n{}", source);
-        let lexer = Lexer::new();
+        let mut lexer = Lexer::new(source);
+        let _tokens = lexer.scan_tokens();
     }
 }
