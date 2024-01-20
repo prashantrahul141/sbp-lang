ade72f6b8c8816cb5700be41b4df2b47e2561611
Prashant <prashantrahul141@protonmail.com>
2024-01-20 15:35:42 +0530

    add error and report utils

--- src/app/utils.rs
+++ src/app/utils.rs
@@ -7,4 +7,12 @@ impl App {
         let default_logger: Arc<Logger> = spdlog::default_logger();
         default_logger.set_level_filter(level);
     }
+
+    pub fn error(&self, line: u32, message: String) {
+        self.report(line, "".to_string(), message)
+    }
+
+    pub fn report(&self, line: u32, where_is: String, message: String) {
+        print!("[line {}] Error {} : {}", line, where_is, message);
+    }
 }
