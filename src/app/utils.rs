5023030aead757e78db2b4044edb89a28bb6e32d
Prashant <prashantrahul141@protonmail.com>
2024-01-24 02:09:38 +0530

    add error_token util for app

--- src/app/utils.rs
+++ src/app/utils.rs
@@ -1,3 +1,5 @@
+use crate::token::{token_main::Token, token_types::TokenType};
+
 use super::app_main::App;
 use spdlog::Logger;
 use std::sync::Arc;
@@ -27,12 +29,26 @@ impl App {
         App::report(line, "".to_string(), message)
     }
 
+    /// Global struct method to display error, its a wrapper for App::repot()
+    /// similar to error, but it takes tokens.
+    /// # Arguments
+    /// * `token` - the token where error occured.
+    /// * `message` - message to display.
+    pub fn error_token(token: Token, message: String) {
+        if token.token_type == TokenType::Eof {
+            App::report(token.line, " at end {}".to_string(), message);
+        } else {
+            App::report(token.line, format!(" at '{}' ", token.lexeme), message);
+        }
+    }
+
     /// Global struct method to display error.
     /// # Arguments
     /// * `line` - the line number in the file error was found.S
     /// * `where_is` - in which phase the error was found.
     /// * `message` - message to display.
     pub fn report(line: usize, where_is: String, message: String) {
+        spdlog::error!("App::report called for line : {line} with where_is : {where_is} and message : {message}");
         println!("[line {}] Error '{}' : {}", line, where_is, message);
     }
 }
