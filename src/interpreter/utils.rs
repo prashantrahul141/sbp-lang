06e0ece85ed34bdfdd72076f8c76eb744faf070f
Prashant <prashantrahul141@protonmail.com>
2024-01-24 23:26:16 +0530

    add: add utils for interpreter

---
+++ src/interpreter/utils.rs
@@ -0,0 +1,31 @@
+use super::interpreter_main::Interpreter;
+use crate::{
+    ast::ast_tree::{walk_expr, Expr},
+    token::token_main::TokenLiterals,
+};
+
+impl Interpreter {
+    /// Constructor for Interpreter.
+    pub fn new() -> Self {
+        Self
+    }
+
+    /// top level public method to start interpretion of an expression.
+    /// # Arguments
+    /// *
+    pub fn interpret(&mut self, expr: Expr) -> TokenLiterals {
+        walk_expr(self, &expr)
+    }
+
+    /// Splax core logic which defines what is truth?
+    /// # Arguments
+    /// * `token_literal` - Token literal to check if its truth or false.
+    pub fn is_truth(&self, token_literal: TokenLiterals) -> TokenLiterals {
+        match token_literal {
+            TokenLiterals::Boolean(value) => TokenLiterals::Boolean(!value),
+            TokenLiterals::Number(value) => TokenLiterals::Boolean(value != 0_f64),
+            TokenLiterals::Null => TokenLiterals::Boolean(false),
+            TokenLiterals::String(value) => TokenLiterals::Boolean(!value.is_empty()),
+        }
+    }
+}
