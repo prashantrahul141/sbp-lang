012665f80e481f76ec7b7b948e355dae2653fe42
Prashant <prashantrahul141@protonmail.com>
2024-01-26 17:07:43 +0530

    make interpreter environment work recursively with enclosing environments

--- src/interpreter/environment.rs
+++ src/interpreter/environment.rs
@@ -5,15 +5,18 @@ use crate::{
 use std::collections::HashMap;
 
 // Top level 'Environment' Data structure to store state of the interpreter.
+#[derive(Debug, Clone)]
 pub struct Environment {
+    pub enclosing: Option<Box<Environment>>,
     pub values: HashMap<String, TokenLiterals>,
 }
 
 impl Environment {
     // constructor.
-    pub fn new() -> Self {
+    pub fn new(enclosing: Option<Box<Environment>>) -> Self {
         spdlog::debug!("creating new environment hashmap.");
         Self {
+            enclosing,
             values: HashMap::new(),
         }
     }
@@ -35,10 +38,18 @@ impl Environment {
     /// * `value` - Assignment value.
     pub fn assign(&mut self, name: Token, value: TokenLiterals) -> Option<TokenLiterals> {
         spdlog::debug!("Assigning value of '{name}' to '{value}'");
+
+        // assign value if the value exists in this environment.
         if self.values.contains_key(&name.lexeme) {
             return self.values.insert(name.lexeme.to_string(), value);
         }
 
+        // recursively find in enclosing environments.
+        if let Some(enclosing) = &mut self.enclosing {
+            return enclosing.assign(name, value);
+        }
+
+        // throw a runtime error if we couldn't find the indentifier.
         App::runtime_error(
             name.line,
             format!("Reference to undefined variable '{}'", name.lexeme),
@@ -51,15 +62,21 @@ impl Environment {
     /// * `name` - The token whose's lexeme value will be searched for.
     pub fn get(&self, name: Token) -> TokenLiterals {
         spdlog::debug!("finding variable with name : {name}");
-        match self.values.get(&name.lexeme) {
-            Some(value) => value.to_owned(),
-            None => {
-                App::runtime_error(
-                    name.line,
-                    format!("Reference to undefined variable '{}'", name.lexeme),
-                );
-                panic!();
-            }
+        // searching the indentifier in the environment itself.
+        if let Some(value) = self.values.get(&name.lexeme) {
+            return value.to_owned();
         }
+
+        // searching the indentifier in enclosing environment.
+        if let Some(enclosing) = &self.enclosing {
+            return enclosing.get(name);
+        }
+
+        // throw a runtime error if we couldn't find the indentifier.
+        App::runtime_error(
+            name.line,
+            format!("Reference to undefined variable '{}'", name.lexeme),
+        );
+        panic!();
     }
 }
